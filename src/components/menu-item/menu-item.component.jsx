01382dc464325d838551fdd723314ee660bd3fbd
Adrian Sanchez <adnsanchez94@gmail.com>
2020-04-23 20:27:06 -0500

    add sections

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div className="background-image" style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <spa className="subtitle">SHOP NOW</spa>
+    </div>
+  </div>
+);
+
+export default MenuItem;
